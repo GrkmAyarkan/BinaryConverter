e87ff5717d651b031a9346c34b74de5c09a4936d
GrkmAyarkan <grkm.ayarkan@gmail.com>
2026-01-11 19:52:11 +0300

    Input and Output

--- BinaryConverter/converter.js
+++ BinaryConverter/converter.js
@@ -1,3 +1,18 @@
 function binaryConverter() {
-    
+    //Input alma
+    const inputBinary = document.getElementById("binaryInput").value;
+
+    // 1. Binary kontrolü
+    if (!/^[01]+$/.test(inputBinary)) {
+        document.getElementById("output").textContent =
+            "Hatalı giriş! Sadece 0 ve 1 girilebilir.";
+        return; // FONKSİYONU DURDURUR
+    }
+
+    // 2. ŞART SAĞLANDIYSA DEVAM
+    //Şimdilik örnek olarak farklı bir işlem yapılacak 
+    let outputNumber = Number(inputBinary)*2
+
+    //Output verme
+    document.getElementById("output").textContent = outputNumber;
 }
